78a06c0257fe9698d3f9d1ecbfd474d6937fcc12
Karen V <karenvo@usc.edu>
2020-01-06 10:02:13 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,6 +2,6 @@ class BirdsController < ApplicationController
   def index
     @birds = Bird.all
     #render 'birds/index.html.erb'
-    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_jsons
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
   end
 end
